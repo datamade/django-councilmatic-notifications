abe1d5305936caf3646e4fb7571a16b137148346
reginafcompton <reginafcompton@gmail.com>
2017-01-09 16:15:53 -0600

    Update release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-councilmatic-notifications',
-    version='0.1.17',
+    version='0.1.18',
     packages=['notifications'],
     include_package_data=True,
     license='MIT License',  # example license

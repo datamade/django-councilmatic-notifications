61b56fd5258dfa4ae258812d2f72a864cbf765b9
Hannah Cushman <hannah.cushman@gmail.com>
2019-04-24 13:48:39 -0500

    Cap rq version below backwards incompatible 1.0

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     author_email='info@datamade.us',
     install_requires=[
         'django-councilmatic>=0.7,<=0.8.7',
+        'rq>=0.5.5,<1.0',
         'django-rq==0.9.3'
     ],
     classifiers=[
